87cccd50bbfee3a74c86f9154521fffe579dfec4
Chris Kalahiki <chris.kalahiki@gmail.com>
2019-08-20 13:08:50 -0500

    asdf

--- bot.py
+++ bot.py
@@ -39,13 +39,13 @@ quotes = ["'Weddings are basically funerals with cake.' - Rick",
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+#     if message.content.startswith('$hello'):
+#         await message.channel.send('Hello!')
 
 @client.event
 async def random_quote(message):
