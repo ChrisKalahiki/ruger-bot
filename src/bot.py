5819e96b414b7105042e274312bfbab3313937f3
ChrisKalahiki <chris.kalahiki@gmail.com>
2021-03-09 15:04:46 -0500

    asdf

--- src/bot.py
+++ src/bot.py
@@ -1,13 +1,6 @@
-import aiohttp
-import asyncio
-import discord
-import logging
-import math
-import random
-import re
-import tweepy
+import aiohttp, asyncio, discord, logging, math, random, re#, json, io#, tweepy
 from googletrans import Translator
-from tweepy import OAuthHandler
+# from tweepy import OAuthHandler
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 logger = logging.getLogger('discord')
@@ -42,47 +35,47 @@ quotes = ["'Weddings are basically funerals with cake.' - Rick",
     "'Well then get your shit together, get it all together and put it in a backpack, all your shit, so it’s together. And if you gotta take it somewhere, take it somewhere, you know. Take it to the shit store and sell it, or put it in the shit museum. I don’t care what you do, you just gotta get it together. Get your shit together.' - Morty"
 ]
 
-class TwitterClient(object):
-	def __init__(self):
-		consumer_key = 'Jc3H9uwEOuJ8xfGZvmI1DzitB'
-		consumer_secret = 'siSOYCxXPnO31PdPVeefjlPA86g6JVVli4UkKKnujCMRzBvGde'
-		access_token = '964329956376567809-Lo9h6A3nougib6POTyx2qku0lLtpzPz'
-		access_token_secret = 'dK64mI0c2Y4NXkc9jI4f6XpqUzxnzEYg1PWzkwDwDakhT'
+# class TwitterClient(object):
+# 	def __init__(self):
+# 		consumer_key = 'Jc3H9uwEOuJ8xfGZvmI1DzitB'
+# 		consumer_secret = 'siSOYCxXPnO31PdPVeefjlPA86g6JVVli4UkKKnujCMRzBvGde'
+# 		access_token = '964329956376567809-Lo9h6A3nougib6POTyx2qku0lLtpzPz'
+# 		access_token_secret = 'dK64mI0c2Y4NXkc9jI4f6XpqUzxnzEYg1PWzkwDwDakhT'
 
-		try:
-			self.auth = OAuthHandler(consumer_key, consumer_secret)
-			self.auth.set_access_token(access_token, access_token_secret)
-			self.api = tweepy.API(self.auth) 
-		except: 
-			print("Error: Authentication Failed") 
+# 		try:
+# 			self.auth = OAuthHandler(consumer_key, consumer_secret)
+# 			self.auth.set_access_token(access_token, access_token_secret)
+# 			self.api = tweepy.API(self.auth) 
+# 		except: 
+# 			print("Error: Authentication Failed") 
 
-	def sentiment_analyzer_scores(self, text):
-		analyzer = SentimentIntensityAnalyzer()
-		translator = Translator()
-		trans = translator.translate(text).text
-		score = analyzer.polarity_scores(trans)
-		lb = score['compound']
-		return lb
+# 	def sentiment_analyzer_scores(self, text):
+# 		analyzer = SentimentIntensityAnalyzer()
+# 		translator = Translator()
+# 		trans = translator.translate(text).text
+# 		score = analyzer.polarity_scores(trans)
+# 		lb = score['compound']
+# 		return lb
 
-	def clean_tweet(self, tweet):
-		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split()) 
+# 	def clean_tweet(self, tweet):
+# 		return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split()) 
 
-	def get_tweets(self, query, count):
-		tweets = [] 
-		try:
-			fetched_tweets = self.api.search(q = query, count = count) 
-			for tweet in fetched_tweets:
-				parsed_tweet = {} 
-				parsed_tweet['text'] = tweet.text
-				parsed_tweet['sentiment'] = self.sentiment_analyzer_scores(self.clean_tweet(tweet.text))
-				if tweet.retweet_count > 0:
-					if parsed_tweet not in tweets: 
-						tweets.append(parsed_tweet) 
-				else: 
-					tweets.append(parsed_tweet) 
-			return tweets 
-		except tweepy.TweepError as e:
-			print("Error : " + str(e)) 
+# 	def get_tweets(self, query, count):
+# 		tweets = [] 
+# 		try:
+# 			fetched_tweets = self.api.search(q = query, count = count) 
+# 			for tweet in fetched_tweets:
+# 				parsed_tweet = {} 
+# 				parsed_tweet['text'] = tweet.text
+# 				parsed_tweet['sentiment'] = self.sentiment_analyzer_scores(self.clean_tweet(tweet.text))
+# 				if tweet.retweet_count > 0:
+# 					if parsed_tweet not in tweets: 
+# 						tweets.append(parsed_tweet) 
+# 				else: 
+# 					tweets.append(parsed_tweet) 
+# 			return tweets 
+# 		except tweepy.TweepError as e:
+# 			print("Error : " + str(e)) 
 
 # create discord client
 client = discord.Client()
@@ -105,15 +98,15 @@ def sentiment_analyzer_scores(text):
 def random_quote():
     return quotes[random.randint(1,len(quotes)) % len(quotes)]
 
-def poll(query, count = 25):
-    api = TwitterClient()
-    tweets = api.get_tweets(query, count) 
-    ptweets = [tweet for tweet in tweets if tweet['sentiment'] >= 0.05]
-    ntweets = [tweet for tweet in tweets if tweet['sentiment'] <= -0.05]
-    a = ("Positive tweets percentage: {} %\n".format(100*len(ptweets)/len(tweets)))
-    b = ("Neutral tweets percentage: {} %\n".format(100*((len(tweets) - len(ntweets) - len(ptweets))/len(tweets))))
-    c = ("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
-    return a + b + c
+# def poll(query, count = 25):
+#     api = TwitterClient()
+#     tweets = api.get_tweets(query, count) 
+#     ptweets = [tweet for tweet in tweets if tweet['sentiment'] >= 0.05]
+#     ntweets = [tweet for tweet in tweets if tweet['sentiment'] <= -0.05]
+#     a = ("Positive tweets percentage: {} %\n".format(100*len(ptweets)/len(tweets)))
+#     b = ("Neutral tweets percentage: {} %\n".format(100*((len(tweets) - len(ntweets) - len(ptweets))/len(tweets))))
+#     c = ("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
+#     return a + b + c
     
 
 @client.event
@@ -127,17 +120,17 @@ async def on_message(message):
     if message.content.startswith('quote'):
         print('quote is firing')
         await message.channel.send(random_quote())
-    elif message.content.startswith('poll'):
-        print('checking twitter')
-        try:
-            results = poll(message.content.split(' ', 2)[2], message.content.split(' ', 2)[1])
-        except IndexError:
-            try:
-                results = poll(message.content.split(' ', 2)[1])
-            except IndexError as e:
-                print('Error: ' + str(e))
-                results = 'Try adding a query after the poll command.'
-        await message.channel.send(results)
+    # elif message.content.startswith('poll'):
+    #     print('checking twitter')
+    #     try:
+    #         results = poll(message.content.split(' ', 2)[2], message.content.split(' ', 2)[1])
+    #     except IndexError:
+    #         try:
+    #             results = poll(message.content.split(' ', 2)[1])
+    #         except IndexError as e:
+    #             print('Error: ' + str(e))
+    #             results = 'Try adding a query after the poll command.'
+    #     await message.channel.send(results)
     else:
         sentiment = sentiment_analyzer_scores(message.content)
         print('sentiment: ' + str(sentiment))
