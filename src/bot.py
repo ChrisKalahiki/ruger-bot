83733d8c0462ab5c05eead890ac251e67f26c019
ChrisKalahiki <chris.kalahiki@gmail.com>
2022-01-18 01:50:24 -0500

    disnake

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,19 @@
 ''' Import Statements '''
-import discord, spotipy, nltk
-import logging, random, json, sys
+import json
+import os
+import platform
+import random
+import sys
+
+import disnake
+from disnake import ApplicationCommandInteraction
+from disnake.ext import tasks, commands
+from disnake.ext.commands import Bot
+from disnake.ext.commands import Context
+
+import exceptions
+import discord
+import logging
 from discord.ext import commands
 from discord_slash import SlashCommand, SlashContext
 
@@ -14,58 +27,182 @@ from discord_slash import SlashCommand, SlashContext
 
 
 ''' Loading Credentials '''
-with open('../pass.json') as f:
-    d = json.load(f)
-
-DISCORD = d['discord']
+if not os.path.isfile("pass.json"):
+    sys.exit("'pass.json' not found! Please add it and try again.")
+else:
+    with open("pass.json") as file:
+        config = json.load(file)
 
 
 ''' Create Discord Bot '''
-description = '''My dog Ruger as a Discord Bot.'''
-bot = commands.Bot(command_prefix='.', description=description, intents=discord.Intents.all())
-slash = SlashCommand(bot, override_type = True)
+intents = disnake.Intents.default()
+
+bot = Bot(command_prefix=config["prefix"], intents=intents)
+# slash = SlashCommand(bot, override_type = True)
 
 
 ''' Discord Bot '''
 @bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+async def on_ready() -> None:
+    print(f"Logged in as {bot.user.name}")
+    print(f"disnake API version: {disnake.__version__}")
+    print(f"Python version: {platform.python_version()}")
+    print(f"Running on: {platform.system()} {platform.release()} ({os.name})")
+    print("-------------------")
+    # status_task.start()
 
-@bot.command()
-async def ping(ctx):
-    """Adds two numbers together."""
-    await ctx.send('pong!')
+@tasks.loop(minutes=1.0)
+async def status_task() -> None:
+    """
+    Setup the game status task of the bot
+    """
+    statuses = ["with you!", "with Krypton!", "with humans!"]
+    await bot.change_presence(activity=disnake.Game(random.choice(statuses)))
 
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
 
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
+if __name__ == "__main__":
+    """
+    This will automatically load slash commands and normal commands located in their respective folder.
+    
+    If you want to remove slash commands, which is not recommended due to the Message Intent being a privileged intent, you can remove the loading of slash commands below.
+    """
+    load_commands("slash")
+    load_commands("normal")
 
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('d'))
-    except Exception:
-        await ctx.send('Format has to be in NdN!')
+@bot.event
+async def on_message(message: disnake.Message) -> None:
+    """
+    The code in this event is executed every time someone sends a message, with or without the prefix
+    :param message: The message that was sent.
+    """
+    if message.author == bot.user or message.author.bot:
         return
-    result = [random.randint(1, limit) for r in range(rolls)]
-    results = f'{ctx.author.name} :game_die:\nResults: ' + str(dice) + ' (' + ', '.join(str(i) for i in result) + ')\nTotal: ' + str(sum(result))
-    await ctx.send(results)
+    await bot.process_commands(message)
 
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
 
-bot.load_extension("cog")
+@bot.event
+async def on_slash_command(interaction: ApplicationCommandInteraction) -> None:
+    """
+    The code in this event is executed every time a slash command has been *successfully* executed
+    :param interaction: The slash command that has been executed.
+    """
+    print(
+        f"Executed {interaction.data.name} command in {interaction.guild.name} (ID: {interaction.guild.id}) by {interaction.author} (ID: {interaction.author.id})")
 
-bot.run(DISCORD)
+
+@bot.event
+async def on_slash_command_error(interaction: ApplicationCommandInteraction, error: Exception) -> None:
+    """
+    The code in this event is executed every time a valid slash command catches an error
+    :param interaction: The slash command that failed executing.
+    :param error: The error that has been faced.
+    """
+    if isinstance(error, exceptions.UserBlacklisted):
+        """
+        The code here will only execute if the error is an instance of 'UserBlacklisted', which can occur when using
+        the @checks.is_owner() check in your command, or you can raise the error by yourself.
+        
+        'hidden=True' will make so that only the user who execute the command can see the message
+        """
+        embed = disnake.Embed(
+            title="Error!",
+            description="You are blacklisted from using the bot.",
+            color=0xE02B2B
+        )
+        print("A blacklisted user tried to execute a command.")
+        return await interaction.send(embed=embed, ephemeral=True)
+    elif isinstance(error, commands.errors.MissingPermissions):
+        embed = disnake.Embed(
+            title="Error!",
+            description="You are missing the permission(s) `" + ", ".join(
+                error.missing_permissions) + "` to execute this command!",
+            color=0xE02B2B
+        )
+        print("A blacklisted user tried to execute a command.")
+        return await interaction.send(embed=embed, ephemeral=True)
+    raise error
+
+
+@bot.event
+async def on_command_completion(context: Context) -> None:
+    """
+    The code in this event is executed every time a normal command has been *successfully* executed
+    :param context: The context of the command that has been executed.
+    """
+    full_command_name = context.command.qualified_name
+    split = full_command_name.split(" ")
+    executed_command = str(split[0])
+    print(
+        f"Executed {executed_command} command in {context.guild.name} (ID: {context.message.guild.id}) by {context.message.author} (ID: {context.message.author.id})")
+
+
+@bot.event
+async def on_command_error(context: Context, error) -> None:
+    """
+    The code in this event is executed every time a normal valid command catches an error
+    :param context: The normal command that failed executing.
+    :param error: The error that has been faced.
+    """
+    if isinstance(error, commands.CommandOnCooldown):
+        minutes, seconds = divmod(error.retry_after, 60)
+        hours, minutes = divmod(minutes, 60)
+        hours = hours % 24
+        embed = disnake.Embed(
+            title="Hey, please slow down!",
+            description=f"You can use this command again in {f'{round(hours)} hours' if round(hours) > 0 else ''} {f'{round(minutes)} minutes' if round(minutes) > 0 else ''} {f'{round(seconds)} seconds' if round(seconds) > 0 else ''}.",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, commands.MissingPermissions):
+        embed = disnake.Embed(
+            title="Error!",
+            description="You are missing the permission(s) `" + ", ".join(
+                error.missing_permissions) + "` to execute this command!",
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    elif isinstance(error, commands.MissingRequiredArgument):
+        embed = disnake.Embed(
+            title="Error!",
+            description=str(error).capitalize(),
+            # We need to capitalize because the command arguments have no capital letter in the code.
+            color=0xE02B2B
+        )
+        await context.send(embed=embed)
+    raise error
+
+# @bot.command()
+# async def ping(ctx):
+#     """Adds two numbers together."""
+#     await ctx.send('pong!')
+
+# @bot.command()
+# async def joined(ctx, member: discord.Member):
+#     """Says when a member joined."""
+#     await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
+
+# @bot.command()
+# async def add(ctx, left: int, right: int):
+#     """Adds two numbers together."""
+#     await ctx.send(left + right)
+
+# @bot.command()
+# async def roll(ctx, dice: str):
+#     """Rolls a dice in NdN format."""
+#     try:
+#         rolls, limit = map(int, dice.split('d'))
+#     except Exception:
+#         await ctx.send('Format has to be in NdN!')
+#         return
+#     result = [random.randint(1, limit) for r in range(rolls)]
+#     results = f'{ctx.author.name} :game_die:\nResults: ' + str(dice) + ' (' + ', '.join(str(i) for i in result) + ')\nTotal: ' + str(sum(result))
+#     await ctx.send(results)
+
+# @bot.command(description='For when you wanna settle the score some other way')
+# async def choose(ctx, *choices: str):
+#     """Chooses between multiple choices."""
+#     await ctx.send(random.choice(choices))
+
+# bot.load_extension("cog")
+
+bot.run(config['discord'])
